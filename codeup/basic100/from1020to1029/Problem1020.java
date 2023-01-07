3c62dd3ff31cf849a81d14587a274d95dfbe353c
whatasame <whatasame@gmail.com>
2023-01-07 19:39:19 +0900

    Solve: CodeUp I/O

---
+++ codeup/basic100/from1020to1029/Problem1020.java
@@ -0,0 +1,29 @@
+package basic100.from1020to1029;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Problem1020 {
+
+    /*
+    * https://codeup.kr/problem.php?id=1020
+    * */
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String input = br.readLine();
+
+        StringTokenizer st = new StringTokenizer(input, "-");
+
+        sb.append(st.nextToken()).append(st.nextToken());
+
+        System.out.println(sb);
+
+        br.close();
+    }
+
+}
