09c1f3dfcdc76418ade79191f39d442a7425fa57
whatasame <whatasame@gmail.com>
2023-01-08 23:22:46 +0900

    Solve: CodeUp I/O

---
+++ codeup/io/Problem1022.java
@@ -0,0 +1,21 @@
+package io;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem1022 {
+
+    /*
+     * https://codeup.kr/problem.php?id=1022
+     * */
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(br.readLine());
+
+        br.close();
+    }
+}
