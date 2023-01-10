e5d38725712aa2e44cd961257933a9a123d63c02
whatasame <whatasame@gmail.com>
2023-01-10 20:43:33 +0900

    Solve: CodeUp I/O

---
+++ codeup/io/Problem1025.java
@@ -0,0 +1,36 @@
+package io;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem1025 {
+
+    /*
+     * https://codeup.kr/problem.php?id=1025
+     * */
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        int length = str.length();
+        int number = Integer.parseInt(str);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = length - 1; i >= 0; i--) {
+            int pow = (int) Math.pow(10, i);
+            int digit = number / pow;
+            number = number - (digit * pow);
+
+            sb.append('[');
+            sb.append(digit).append(Integer.toString(pow).substring(1));
+            sb.append(']').append('\n');
+        }
+
+        System.out.println(sb);
+        br.close();
+
+    }
+}
+
