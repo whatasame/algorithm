622d2ac36d27e0e983e1708a5bcdf7bc8902c6a9
whatasame <whatasame@gmail.com>
2023-01-16 23:41:19 +0900

    Solve: Backjoon Math

---
+++ baekjoon/math/Problem1929.java
@@ -0,0 +1,46 @@
+package math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Problem1929 {
+
+    /*
+     * https://www.acmicpc.net/problem/1929
+     * */
+
+    public static boolean isPrime(int num) {
+        if (num == 1) {
+            return true;
+        }
+
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = M; i <= N; i++) {
+            if (isPrime(i)) {
+                sb.append(i).append('\n');
+            }
+        }
+
+        System.out.println(sb);
+
+        br.close();
+    }
+}
