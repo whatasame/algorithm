6adfc9979740abcb839ec3e5a40fd83b2cb4f55c
whatasame <whatasame@gmail.com>
2023-01-24 16:57:16 +0900

    Solve: use Biginteger.gcd()

--- baekjoon/math/Problem2824.java
+++ baekjoon/math/Problem2824.java
@@ -3,6 +3,7 @@ package math;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.*;
 
 public class Problem2824 {
@@ -11,11 +12,6 @@ public class Problem2824 {
      * https://www.acmicpc.net/problem/2824
      * */
 
-    public static Map<Integer, Integer> primeCountOfA = new HashMap<>(); // prime factorization of A
-
-    public static Map<Integer, Integer> primeCountOfB = new HashMap<>(); // prime factorization of B
-
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
@@ -23,43 +19,24 @@ public class Problem2824 {
         // Input data of A
         int N = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine(), " ");
-        List<Integer> numListA = new LinkedList<>();
+        BigInteger A = BigInteger.ONE;
         for (int i = 0; i < N; i++) {
-            numListA.add(Integer.valueOf(st.nextToken()));
+            A = A.multiply(new BigInteger(st.nextToken()));
         }
 
         // Input data of B
         int M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine(), " ");
-        List<Integer> numListB = new LinkedList<>();
+        BigInteger B = BigInteger.ONE;
         for (int i = 0; i < M; i++) {
-            numListB.add(Integer.valueOf(st.nextToken()));
+            B = B.multiply(new BigInteger(st.nextToken()));
         }
-
-        // Prime factorization
-        for (Integer num : numListA) {
-            setPrimeCountMap(primeCountOfA, num);
-        }
-        for (Integer num : numListB) {
-            setPrimeCountMap(primeCountOfB, num);
-        }
-
-        // Intersection key of prime
-        Set<Integer> primeOfA = primeCountOfA.keySet();
-        Set<Integer> primeOfB = primeCountOfB.keySet();
-        primeOfA.retainAll(primeOfB); // now primeOfA is intersection set
 
         // get GCD
-        long GCD = 1;
-        for (Integer num : primeOfA) {
-            int minCount = getMinVal(num);
-            for (int i = 0; i < minCount; i++) {
-                GCD = GCD * num;
-            }
-        }
+        BigInteger GCD = A.gcd(B);
 
         // print GCD
-        String result = String.valueOf(GCD);
+        String result = GCD.toString();
         if (result.length() > 9) {
             result = result.substring(result.length() - 9);
         }
@@ -68,20 +45,4 @@ public class Problem2824 {
         br.close();
     }
 
-    private static int getMinVal(Integer num) {
-        int valueOfA = primeCountOfA.getOrDefault(num, 0);
-        int valueOfB = primeCountOfB.getOrDefault(num, 0);
-
-        return Math.min(valueOfA, valueOfB);
-    }
-
-    private static void setPrimeCountMap(Map<Integer, Integer> pf, Integer target) {
-        for (int i = 2; i <= Math.sqrt(target); i++) {
-            while (target % i == 0) {
-                pf.put(i, pf.getOrDefault(i, 0) + 1); // (value count of dictionary)++
-                target = target / i;
-            }
-        }
-        pf.put(target, pf.getOrDefault(target, 0) + 1); // target is 1 or prime number
-    }
 }
