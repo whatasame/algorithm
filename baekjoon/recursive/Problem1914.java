eb126236b4713874452a8e24a403bfd80c93943a
whatasame <whatasame@gmail.com>
2023-02-08 02:17:47 +0900

    Solve: exception handling if N is greater than or equal to 20

--- baekjoon/recursive/Problem1914.java
+++ baekjoon/recursive/Problem1914.java
@@ -3,11 +3,11 @@ package recursive;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 
 public class Problem1914 {
 
     private static final StringBuilder sb = new StringBuilder();
-    private static boolean genString; // Flag for string generation
     private static int moveCount = 0;
 
     public static void main(String[] args) throws IOException {
@@ -15,22 +15,28 @@ public class Problem1914 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        /* Init flag */
-        genString = N <= 20;
+        /* Branch by N */
+        if (N <= 20) {
+            /* Run algorithm */
+            hanoiTower(N, 1, 3, 2);
 
-        /* Run algorithm */
-        hanoiTower(N, 1, 3, 2);
+            /* Print result */
+            System.out.println(moveCount);
+            System.out.println(sb);
+        } else {
+            BigInteger two = new BigInteger("2");
+            BigInteger count = two.pow(N).subtract(BigInteger.ONE); // 2^N - 1
 
-        /* Print result */
-        System.out.println(moveCount);
-        System.out.println(sb);
+            /* Print result */
+            System.out.println(count);
+        }
 
         br.close();
     }
 
     /**
-     * Append hanoi tower path if genString is true
-     * Increase moveCount by one
+     * Append hanoi tower path if height of tower is less than or equal 20
+     * and also increase moveCount by one
      *
      * @param height height of tower
      * @param from   start rod
@@ -42,17 +48,13 @@ public class Problem1914 {
         moveCount++;
 
         if (height == 1) { // End of recursive
-            if (genString) {
-                sb.append(from).append(' ').append(to).append('\n');
-            }
+            sb.append(from).append(' ').append(to).append('\n');
             return;
         }
 
         /* Recursive (Move N-1 tower) */
         hanoiTower(height - 1, from, tmp, to);
-        if (genString) {
-            sb.append(from).append(' ').append(to).append('\n');
-        }
+        sb.append(from).append(' ').append(to).append('\n');
         hanoiTower(height - 1, tmp, to, from);
     }
 }
