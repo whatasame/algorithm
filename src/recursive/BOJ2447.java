98f93d46ab22c31805f3e299a745171085592cd8
whatasame <whatasame@gmail.com>
2023-02-10 15:30:02 +0900

    Improve: use static array and remove init data

--- src/recursive/BOJ2447.java
+++ src/recursive/BOJ2447.java
@@ -6,22 +6,19 @@ import java.io.InputStreamReader;
 
 public class BOJ2447 {
 
+    private static char[][] arr;
+
 
     public static void main(String[] args) throws IOException {
         /* Read input N */
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        /* Init array */
-        char[][] arr = new char[N][N];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                arr[i][j] = '*';
-            }
-        }
+        /* Create array instance */
+        arr = new char[N][N];
 
         /* Run recursive */
-        fillArr(arr, 0, 0, N, false);
+        fillArr(0, 0, N, false);
 
         /* Print result */
         StringBuilder sb = new StringBuilder();
@@ -36,9 +33,10 @@ public class BOJ2447 {
         br.close();
     }
 
-    private static void fillArr(char[][] arr, int row, int col, int length, boolean whiteSpace) {
+    private static void fillArr(int row, int col, int length, boolean whiteSpace) {
         /* End of recursive */
         if (length < 1) {
+            arr[row][col] = '*';
             return;
         }
 
@@ -52,18 +50,18 @@ public class BOJ2447 {
         }
 
         /* Top */
-        fillArr(arr, row, col, length / 3, false);
-        fillArr(arr, row, col + length / 3, length / 3, false);
-        fillArr(arr, row, col + 2 * length / 3, length / 3, false);
+        fillArr(row, col, length / 3, false);
+        fillArr(row, col + length / 3, length / 3, false);
+        fillArr(row, col + 2 * length / 3, length / 3, false);
 
         /* Middle */
-        fillArr(arr, row + length / 3, col, length / 3, false);
-        fillArr(arr, row + length / 3, col + length / 3, length / 3, true);
-        fillArr(arr, row + length / 3, col + 2 * length / 3, length / 3, false);
+        fillArr(row + length / 3, col, length / 3, false);
+        fillArr(row + length / 3, col + length / 3, length / 3, true);
+        fillArr(row + length / 3, col + 2 * length / 3, length / 3, false);
 
         /* Bottom */
-        fillArr(arr, row + 2 * length / 3, col, length / 3, false);
-        fillArr(arr, row + 2 * length / 3, col + length / 3, length / 3, false);
-        fillArr(arr, row + 2 * length / 3, col + 2 * length / 3, length / 3, false);
+        fillArr(row + 2 * length / 3, col, length / 3, false);
+        fillArr(row + 2 * length / 3, col + length / 3, length / 3, false);
+        fillArr(row + 2 * length / 3, col + 2 * length / 3, length / 3, false);
     }
 }
