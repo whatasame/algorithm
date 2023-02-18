f1ff9b842c8b65ebdbd5b195f5e381b66a39db95
whatasame <whatasame@gmail.com>
2023-02-18 20:03:44 +0900

    Improve: create reverse sequence and use LIS

--- src/dp/BOJ11722.java
+++ src/dp/BOJ11722.java
@@ -8,33 +8,33 @@ import java.util.StringTokenizer;
 public class BOJ11722 {
 
     public static void main(String[] args) throws IOException {
-        /* Init sequence  */
+        /* Init reverse sequence  */
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         String numStr = br.readLine();
         int[] sequence = new int[N];
         StringTokenizer st = new StringTokenizer(numStr, " ");
-        for (int i = 0; i < N; i++) {
+        for (int i = N - 1; i >= 0; i--) {
             sequence[i] = Integer.parseInt(st.nextToken());
         }
 
-        /* LDS algorithm */
-        int[] LDS = new int[N];
-        LDS[0] = 1;
+        /* LIS algorithm */
+        int[] LIS = new int[N];
+        LIS[0] = 1;
         for (int i = 1; i < N; i++) {
-            // Compute max of LDS[0, ..., i-1]
+            // Compute max of LIS[0, ..., i-1]
             int max = 0;
             for (int j = 0; j < i; j++) {
-                if (sequence[j] > sequence[i]) {
-                    max = Math.max(LDS[j], max);
+                if (sequence[j] < sequence[i]) {
+                    max = Math.max(LIS[j], max);
                 }
             }
-            LDS[i] = max + 1;
+            LIS[i] = max + 1;
         }
 
         /* Print result */
         int result = 0;
-        for (int length : LDS) {
+        for (int length : LIS) {
             result = Math.max(result, length);
         }
         System.out.println(result);
