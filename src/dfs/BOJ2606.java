e6aa68ee1f5b4196fbd1fb0860b472ff063808f4
whatasame <whatasame@gmail.com>
2023-03-02 17:28:46 +0900

    Wrong: only sample cases passed

---
+++ src/dfs/BOJ2606.java
@@ -0,0 +1,59 @@
+package dfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ2606 {
+
+    private static boolean[][] graph;
+
+    private static boolean[] visited;
+
+    private static int count = -1;
+
+    public static void main(String[] args) throws IOException {
+        /* Read input N, E */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int E = Integer.parseInt(br.readLine());
+
+        /* Init directed graph */
+        graph = new boolean[N + 1][N + 1];
+        while (E-- > 0) {
+            /* Read edge<u, v> */
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            /* Add edge<u, v> */
+            graph[u][v] = true;
+        }
+
+        /* Infect computer connected with no.1 */
+        visited = new boolean[N + 1];
+        dfs(1);
+
+        /* Print result */
+        System.out.println(count);
+
+        br.close();
+    }
+
+    public static void dfs(int vertex) {
+        if (visited[vertex]) {
+            return;
+        }
+
+        visited[vertex] = true;
+        count++;
+
+        for (int i = 1; i < graph[vertex].length; i++) {
+            if (graph[vertex][i]) {
+                dfs(i);
+            }
+        }
+
+    }
+}
