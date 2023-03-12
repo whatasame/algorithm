cbe5ccef084c08ba77d143f26d3355459fadef92
whatasame <whatasame@gmail.com>
2023-03-12 15:33:19 +0900

    Improve: use String.replaceAll()
    more readability, less performance
    Mem: 11516KB -> 11648KB (1.14% increased)
    Execution time: 76ms -> 80ms  (5.26% increased)

--- src/greedy/BOJ1343.java
+++ src/greedy/BOJ1343.java
@@ -6,54 +6,24 @@ import java.io.InputStreamReader;
 
 public class BOJ1343 {
 
-    private static StringBuilder sb = new StringBuilder();
-
     public static void main(String[] args) throws IOException {
-        /* Read input str */
+        /* Read input boardStr */
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        final char[] str = br.readLine().toCharArray();
+        String boardStr = br.readLine();
 
-        /* Cover string */
-        int countX = 0;
-        for (char c : str) {
-            if (c == '.') {
-                if (countX % 2 != 0) {
-                    sb = new StringBuilder("-1");
-                    break;
-                } else {
-                    cover(countX);
-                }
+        /* Replace boardStr's "XXXX" to "AAAA" */
+        boardStr = boardStr.replaceAll("XXXX", "AAAA");
 
-                sb.append('.');
-                countX = 0;
-            } else if (c == 'X') {
-                countX++;
-            }
-        }
-
-        /* Cover last substring */
-        if (countX % 2 != 0) {
-            sb = new StringBuilder("-1");
-        } else {
-            cover(countX);
-        }
+        /* Replace boardStr's "XX" to "BB" */
+        boardStr = boardStr.replaceAll("XX", "BB");
 
         /* Print result */
-        System.out.println(sb);
+        if (boardStr.contains("X")) {
+            System.out.println(-1);
+        } else {
+            System.out.println(boardStr);
+        }
 
         br.close();
     }
-
-    public static void cover(int countX) {
-        while (countX != 0) {
-            if (countX == 2) {
-                sb.append("BB");
-                countX -= 2;
-            } else {
-                sb.append("AAAA");
-                countX -= 4;
-            }
-        }
-    }
-
 }
