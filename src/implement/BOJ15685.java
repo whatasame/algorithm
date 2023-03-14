f30c141b12a5d58650dc8b67b4a5ee2f24d88a22
whatasame <whatasame@gmail.com>
2023-03-14 23:43:56 +0900

    Unsolved: misunderstanding of Gen 0 dragon cover

--- src/implement/BOJ15685.java
+++ src/implement/BOJ15685.java
@@ -3,99 +3,94 @@ package implement;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class BOJ15685 {
 
-    private static final int ARRAY_LENGTH = 100;
+    private static final int RIGHT = 0;
+    private static final int UP = 1;
+    private static final int LEFT = 2;
+    private static final int DOWN = 3;
+    private static final int ARRAY_LENGTH = 101;
 
-    private static boolean[][] arr;
+    private static final boolean[][] arr = new boolean[ARRAY_LENGTH][ARRAY_LENGTH];
 
     public static void main(String[] args) throws IOException {
         /* Read input N */
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         final int N = Integer.parseInt(br.readLine());
 
-        /* Init arr */
-        arr = new boolean[ARRAY_LENGTH][ARRAY_LENGTH];
-
         /* Draw dragon cover */
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             int startX = Integer.parseInt(st.nextToken());
             int startY = Integer.parseInt(st.nextToken());
-            int direction = Integer.parseInt(st.nextToken());
-            int generation = Integer.parseInt(st.nextToken());
+            int dir = Integer.parseInt(st.nextToken());
+            int gen = Integer.parseInt(st.nextToken());
 
-            draw(startX, startY, direction, generation);
-        }
-
-        /* Count rectangle */
-        int count = 0;
-        for (int i = 0; i < ARRAY_LENGTH; i++) {
-            for (int j = 0; j < ARRAY_LENGTH; j++) {
-                if (arr[i][j]
-                        && isValidIndex(i + 1, j + 1) // Check valid index
-                        && arr[i + 1][j] && arr[i][j + 1] && arr[i + 1][j + 1]) { // Check rectangle
-                    count++;
-                }
-
-            }
+            draw(startX, startY, getDirections(dir, gen));
         }
 
         /* Print result */
-        System.out.println(count);
+        int result = getCountOfRectangle();
+        System.out.println(result);
 
         br.close();
     }
 
-    public static void draw(int startX, int startY, int direction, int generation) {
-        Stack<Integer> directionStack = new Stack<>();
-        directionStack.push(direction);
 
-        for (int i = 0; i < generation; i++) {
-            /* Copy directionStack */
-            Stack<Integer> tmp = new Stack<>();
-            for (Integer dir : directionStack) {
-                tmp.push(dir);
-            }
+    public static List<Integer> getDirections(int firstDir, int generation) {
+        List<Integer> directions = new ArrayList<>();
+        directions.add(firstDir);
 
-            /* Generate dragon cover */
-            while (!directionStack.empty()) {
-                tmp.push((directionStack.pop() + 1) % 4);
+        /* Generate dragon cover direction list */
+        while (generation-- > 0) {
+            for (int i = directions.size() - 1; i >= 0; i--) {
+                int newDir = (directions.get(i) + 1) % 4;
+                directions.add(newDir);
             }
-            directionStack = tmp;
         }
 
+        return directions;
+    }
+
+    public static void draw(int x, int y, List<Integer> directions) {
         /* Init start point */
-        int x = startX;
-        int y = startY;
-        arr[y][x] = true;
+        arr[x][y] = true;
 
-        /* Draw dragon cover */
-        for (int dir : directionStack) {
-            switch (dir) { // Compute position
-                case 0:
-                    x++;
+        /* Draw point of dragon cover */
+        for (int dir : directions) {
+            switch (dir) {
+                case RIGHT:
+                    arr[++x][y] = true;
                     break;
-                case 1:
-                    y--;
+                case UP:
+                    arr[x][--y] = true;
                     break;
-                case 2:
-                    x--;
+                case LEFT:
+                    arr[--x][y] = true;
                     break;
-                case 3:
-                    y++;
+                case DOWN:
+                    arr[x][++y] = true;
                     break;
-            }
-            if (isValidIndex(y, x)) {
-                arr[y][x] = true; // draw
             }
         }
     }
 
-    public static boolean isValidIndex(int i, int j) {
-        return i < ARRAY_LENGTH && j < ARRAY_LENGTH;
+
+    public static int getCountOfRectangle() {
+        int count = 0;
+
+        for (int x = 0; x < ARRAY_LENGTH - 1; x++) {
+            for (int y = 0; y < ARRAY_LENGTH - 1; y++) {
+                if (arr[x][y] && arr[x + 1][y] && arr[x][y + 1] && arr[x + 1][y + 1])
+                    count++;
+            }
+        }
+
+        return count;
     }
+
 }
