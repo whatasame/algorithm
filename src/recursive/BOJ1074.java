f57518c476ceb2f90307aadddf620200c7e06d31
whatasame <whatasame@gmail.com>
2023-03-18 18:07:25 +0900

    wrong: Time Limit Exceeded

---
+++ src/recursive/BOJ1074.java
@@ -0,0 +1,61 @@
+package recursive;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ1074 {
+
+    private static int r;
+    private static int c;
+
+    private static boolean isFound = false;
+    private static int count = -1;
+    private static final int[] offsetRow = {0, 0, 1, 1};
+    private static final int[] offsetCol = {0, 1, 0, 1};
+
+    public static void main(String[] args) throws IOException {
+        /* Read input N, r, c */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        final int N = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+
+        /* Visit recursively */
+        int length = (int) Math.pow(2, N);
+        visit(0, 0, length);
+
+        /* Print result */
+        System.out.println(count);
+
+        br.close();
+    }
+
+    private static void visit(int row, int col, int length) {
+        if (isFound) {
+            return;
+        }
+
+        if (length <= 2) {
+            for (int i = 0; i < offsetCol.length; i++) {
+                count++;
+
+                int newRow = row + offsetRow[i];
+                int newCol = col + offsetCol[i];
+
+                if (newRow == r && newCol == c) {
+                    isFound = true;
+                    return;
+                }
+            }
+            return;
+        }
+
+        visit(row, col, length / 2);
+        visit(row, col + length / 2, length / 2);
+        visit(row + length / 2, col, length / 2);
+        visit(row + length / 2, col + length / 2, length / 2);
+    }
+}
