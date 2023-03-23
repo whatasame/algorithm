d496b5a674e18eb62b12a257f3528a968c499d8f
whatasame <whatasame@gmail.com>
2023-03-23 16:53:31 +0900

    improve: more readable

--- src/string/BOJ1718.java
+++ src/string/BOJ1718.java
@@ -6,35 +6,50 @@ import java.io.InputStreamReader;
 
 public class BOJ1718 {
 
+    private static final int MAX_ALPHABET_NUM = 26;
+
     public static void main(String[] args) throws IOException {
         /* Read input PLAIN, KEY */
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        final char[] PLAIN = br.readLine().toCharArray();
-        final char[] KEY = br.readLine().toCharArray();
+        final String PLAIN = br.readLine();
+        final String KEY = br.readLine();
 
-        /* Generate cypher */
-        StringBuilder sb = new StringBuilder();
-        int idx = 0;
-        for (char p : PLAIN) {
-            if (p == ' ') {
-                sb.append(' ');
-            } else {
-                /* Compute encrypted character */
-                int interval = KEY[idx] - 'a' + 1;
-
-                int encrypted = p - interval;
-                encrypted = encrypted < 'a' ? ('z' + 1) - ('a' - encrypted) : encrypted;
-
-                sb.append((char) encrypted);
-            }
-
-            idx = (idx + 1) % KEY.length;
-        }
+        /* Run algorithm */
+        String cypher = encrypt(PLAIN, KEY);
 
         /* Print result */
-        System.out.println(sb);
+        System.out.println(cypher);
 
         br.close();
     }
+
+    private static String encrypt(String plain, String key) {
+        StringBuilder cypher = new StringBuilder();
+
+        for (int i = 0; i < plain.length(); i++) {
+            char p = plain.charAt(i);
+            char k = key.charAt(i % key.length());
+
+            if (p == ' ') {
+                cypher.append(p);
+            } else {
+                /* Alphabet number starts with 1 */
+                int plainNum = p - 'a' + 1;
+                int keyNum = k - 'a' + 1;
+
+                int encrypted = plainNum - keyNum;
+                if (encrypted <= 0) {
+                    encrypted = MAX_ALPHABET_NUM + encrypted;
+                }
+
+                encrypted--; // because 'a' starts with 0, not starts with 1
+
+                cypher.append((char) ('a' + encrypted)); // 'a' = 'a' + 0, not 'a' + 1
+            }
+        }
+
+        return cypher.toString();
+    }
+
 
 }
